b9705570ba215ee8125c1dba352283cbea133549
Rajkumar Bantu <76648291+RajkumarBantu@users.noreply.github.com>
2024-03-28 01:19:01 -0400

    Add files via upload

---
+++ attempt3.cmd
@@ -0,0 +1,17 @@
+
+stepsize 100
+
+vector In1 Input 1
+vector In2 Input 2
+vector In3 Input 3
+
+analyzer In1 Output 1
+analyzer In2 Output 2
+
+
+setvector In1 00001 
+setvector In2 01000
+setvector In3 10101
+s
+
+
